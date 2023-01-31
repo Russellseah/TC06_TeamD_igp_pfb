7d5cb78cec6f42b08509ae0c756b8e9c1ea1af55
Russellseah <S10242192@connect.np.edu.sg>
2023-01-31 12:40:28 +0800

    new file:   igp_pfb/cash_on_hand.py 	new file:   igp_pfb/csv_reports/cash-on-hand.csv 	new file:   igp_pfb/csv_reports/overheads.csv 	new file:   igp_pfb/csv_reports/profit-and-loss.csv 	new file:   igp_pfb/main.py 	new file:   igp_pfb/overheads.py 	new file:   igp_pfb/profit_loss.py

---
+++ igp_pfb/main.py
@@ -0,0 +1,15 @@
+import cash_on_hand, overheads, profit_loss
+
+def main():
+    """
+    Main function that imports necessary modules and performs calculations
+    """
+    # Import the overheads module and call the overheads_function
+    overheads.overheads_function()
+    # Import the cash_on_hand module and call the coh_function
+    cash_on_hand.coh_function()
+    # Import the profit_loss module and call the profitloss_function
+    profit_loss.profitloss_function()
+
+# Call the main function
+main()
